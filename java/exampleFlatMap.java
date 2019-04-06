dd09f6d18b637f41ca8ace27b859f87a1e598da2
Nazeel <nazeel.ak@gmail.com>
2019-04-06 08:54:59 +0530

    java flatmap

---
+++ java/exampleFlatMap.java
@@ -0,0 +1,21 @@
+package com.nazeel;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class App 
+{
+    public static void main( String[] args ) {
+
+        Map<String, List<String>> people = new HashMap<>();
+        people.put("John", Arrays.asList("555-1123", "555-3389"));
+        people.put("Mary", Arrays.asList("555-2243", "555-5264"));
+        people.put("Steve", Arrays.asList("555-6654", "555-3242"));
+
+        List<String> phones = people.values().stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+
+
+}
